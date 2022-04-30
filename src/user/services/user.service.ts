421c705d6196e34c9eb891708cdeb15f297d2010
robyngoodner <robyn.goodner@gmail.com>
2022-04-30 16:49:17 -0400

    Adds full CRUD on userStatus model

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { from, Observable } from 'rxjs';
+import { Repository, UpdateResult, DeleteResult } from 'typeorm';
+
+import { UserStatusEntity } from '../models/user.entity'
+import { UserStatus } from '../models/user.interface'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(UserStatusEntity)
+        private readonly userStatusRepository: Repository<UserStatusEntity>
+    ) {}
+
+    createUser(userStatus: UserStatus): Observable<UserStatus> {
+        return from(this.userStatusRepository.save(userStatus));
+    }
+
+    findAllUsers(): Observable<UserStatus[]> {
+        return from(this.userStatusRepository.find());
+    }
+
+    updateUser(id: number, userStatus: UserStatus): Observable<UpdateResult> {
+        return from(this.userStatusRepository.update(id, userStatus))
+    }
+
+    deleteUser(id: number): Observable<DeleteResult> {
+        return from(this.userStatusRepository.delete(id));
+    }
+}
