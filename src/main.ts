756e724bf92af31db225701915f9c08a2e870ab4
robyngoodner <robyn.goodner@gmail.com>
2022-04-30 17:04:10 -0400

    Edits port/host

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const HOST = 'localhost'
+const PORT = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(PORT, HOST, () => {
+    console.log(`Listening on http://${HOST}:${PORT}`);
+  });
 }
 bootstrap();
